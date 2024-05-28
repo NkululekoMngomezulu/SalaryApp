63171e37da97e1438e1ec21a55a2bc6c4d64cbbb
nmngomezulu <nmngomezulu@SWC-ADD228-06D>
2024-05-28 15:25:27 +0200

    Add project files.

---
+++ SalaryApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SalaryApp;
+
+class SalaryApp
+{
+    public static void Main(string[] args)
+    {
+        int hours;
+        double rate, salary;
+
+        Console.WriteLine("enter hours");
+        hours = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("enter Rate");
+        rate = Convert.ToDouble(Console.ReadLine());
+
+        salary = rate * hours;
+        Console.WriteLine("your salary is:" + salary);
+    }
+}
